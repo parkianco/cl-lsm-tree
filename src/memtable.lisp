5758adfd9a1d52d31b7ea6420c9c47a04afd62c6
Park Ian Co <opensource@parkian.co>
2026-03-15 00:09:37 -0400

    Relicense from BSD-3-Clause to Apache-2.0

--- src/memtable.lisp
+++ src/memtable.lisp
@@ -1,5 +1,5 @@
 ;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;;;; SPDX-License-Identifier: BSD-3-Clause
+;;;; SPDX-License-Identifier: Apache-2.0
 
 ;;;; memtable.lisp - In-memory sorted table with skip list
 ;;;;
